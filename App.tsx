fc0980b180df6f56ae97fd440fec536a4c568611
Maíra Costa <mairaccst@gmail.com>
2025-02-03 21:54:48 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto"; //npx expo install expo-font @expo-googgle-fonts/roboto
+import { Center, GluestackUIProvider, Text } from "@gluestack-ui/themed"; // npm install @gluestack-ui/themed@1.1.34 @gluestack-style/react@1.0.57 @gluestack-ui/config@1.1.19 --legacy-peer-deps
+
+/*
+ import { config } from "@gluestack-ui/config"; // config permite usar o tema padrão do gluestack
+ 
+*/
+
+import { config } from "./config/gluestack-ui.config"; // npx gluestack-ui-scripts eject-theme => cria a pasta config, que contem o arquivo gluestack-ui.config.ts com todos os tokens, que podem ser customizados
+
+import { Loading } from "@components/Loading";
+import { SignUp } from "@screens/SignUp";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignUp /> : <Loading />}
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,63 @@
+import {
+  VStack,
+  Image,
+  Center,
+  Text,
+  Heading,
+  ScrollView,
+} from "@gluestack-ui/themed";
+
+import BackGroundImage from "@assets/background.png"; // Para reconhecer a importação é preciso cria o arquivo png.d.ts
+/*
+ Para usar svg instalamos duas bibliotecas: 
+  1) npx expo install react-native-svg  
+  2) npm install --save-dev react-native-svg-transformer
+   - Criar e configurar arquivo metro.config.js de acordo com doc https://github.com/kristerkari/react-native-svg-transformer
+*/
+import Logo from "@assets/logo.svg"; // Para reconhecer a importação é preciso cria o arquivo svg.d.ts com cofiguração de acordo com doc https://github.com/kristerkari/react-native-svg-transformer
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignIn() {
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w="$full"
+          h={624}
+          source={BackGroundImage}
+          defaultSource={BackGroundImage}
+          alt="Pessoas treinando"
+          position="absolute"
+        />
+        <VStack flex={1} px="$10" pb="$16">
+          <Center my="$24">
+            <Logo />
+            <Text color="$gray100" fontSize="$sm">
+              Treine a sua mente e o seu corpo.
+            </Text>
+          </Center>
+          <Center gap="$2">
+            <Heading color="$gray100">Acesse sua conta</Heading>
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Senha" secureTextEntry />
+            <Button title="Acessar" />
+          </Center>
+          <Center flex={1} justifyContent="flex-end" mt="$4">
+            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+              Ainda não tem acesso?
+            </Text>
+            <Button title="Criar Conta" variant="outline" />
+          </Center>
+        </VStack>
+      </VStack>
+    </ScrollView>
+  );
+}
