51c57e7221abb133a35590b29b958f13f4bdcc40
Maíra Costa <mairaccst@gmail.com>
2025-02-07 15:58:29 -0300

    adds private routes and tab navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,17 @@ import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Box } from "@gluestack-ui/themed";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
-import { gluestackUIConfig } from "@gluestack-ui/config";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 
 export function Routes() {
   const theme = DefaultTheme; //Usa a cor padrão do tema no react navigation
-  theme.colors.background = gluestackUIConfig.tokens.colors.blueGray700; //customiza background - não apareceu o gray 700
+  theme.colors.background = gluestackUIConfig.tokens.colors.gray700; //customiza background - não apareceu o gray 700
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   );
