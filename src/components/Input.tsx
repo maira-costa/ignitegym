318fc0c3fee2f716f08c69844f4642f335d6cfc0
Maíra Costa <mairaccst@gmail.com>
2025-02-11 18:29:29 -0300

    adds History and Profile screens components

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,26 @@
 import { ComponentProps } from "react";
 import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
 
-type Props = ComponentProps<typeof InputField>; // Necessário para acessar as propriedades do GluestackInput pelo componente customizado Input
+type Props = ComponentProps<typeof InputField> & {
+  isReadOnly?: boolean;
+}; // Necessário para acessar as propriedades do GluestackInput pelo componente customizado Input
 
-export function Input({ ...rest }: Props) {
+export function Input({ isReadOnly = false, ...rest }: Props) {
   return (
     <GluestackInput
-      bg="$gray700"
       h="$14"
-      px="$4"
       borderWidth="$0"
       borderRadius="$md"
       $focus={{
         borderWidth: 1,
         borderColor: "$green500",
       }}
+      isReadOnly={isReadOnly}
+      opacity={isReadOnly ? 0.3 : 1}
     >
       <InputField
+        px="$4"
+        bg="$gray700"
         color="$white"
         fontFamily="$body"
         placeholder="$gray300"
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,38 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
+
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Input } from "@components/Input";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="$white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: "https://github.com/maira-costa.png" }}
+            alt="Foto do usuário"
+            size="xl"
+          />
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="maira@email.com" bg="$gray600" isReadOnly />
+          </Center>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
